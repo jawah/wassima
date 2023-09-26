8398d6c40cb77428bbc86416378b9c0ae297c480
TAHRI Ahmed R <Ousret@users.noreply.github.com>
2023-09-26 14:03:54 +0200

    :sparkle: Universal build fallback with certifi (#1)

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -3,6 +3,12 @@
 All notable changes to wassima will be documented in this file. This project adheres to [Semantic Versioning](https://semver.org/spec/v2.0.0.html).
 The format is based on [Keep a Changelog](https://keepachangelog.com/en/1.0.0/).
 
+## 1.0.1 (2023-09-26)
+
+### Added
+- Expose `__version__`.
+- Support for `certifi` fallback if you did not pick up a compatible wheel. Expose constant `RUSTLS_LOADED` as a witness.
+
 ## 1.0.0 (2023-09-20)
 
 ### Added
--- wassima/__init__.py
+++ wassima/__init__.py
@@ -8,7 +8,49 @@ import ssl
 from functools import lru_cache
 from ssl import DER_cert_to_PEM_cert
 
-from ._rustls import root_der_certificates
+from ._version import VERSION, __version__
+
+#: Determine if we could load correctly the non-native rust module.
+RUSTLS_LOADED: bool
+
+try:
+    from ._rustls import root_der_certificates
+
+    RUSTLS_LOADED = True
+except ImportError:
+    RUSTLS_LOADED = False
+    from ssl import PEM_cert_to_DER_cert
+
+    try:
+        import certifi  # type: ignore
+    except ImportError:
+        certifi = None
+
+    if certifi is None:
+        import platform
+        import warnings
+
+        warnings.warn(
+            f"""Unable to access your system root CAs. Your particular interpreter and/or
+            operating system ({platform.python_implementation()}, {platform.uname()}, {platform.python_version()})
+            is not be supported. While it is not ideal, you may circumvent that warning by having certifi
+            installed in your environment. Run `python -m pip install certifi`.
+            You may also open an issue at https://github.com/jawah/wassima/issues to get your platform compatible.""",
+            RuntimeWarning,
+        )
+
+    @lru_cache()
+    def root_der_certificates() -> list[bytes]:
+        if certifi is None:
+            return []
+
+        certs: list[bytes] = []
+
+        with open(certifi.where(), encoding="utf-8") as fp:
+            for pem_cert in fp.read().split("\n\n"):
+                certs.append(PEM_cert_to_DER_cert(pem_cert))
+
+        return certs
 
 
 @lru_cache()
@@ -45,6 +87,7 @@ def create_default_ssl_context() -> ssl.SSLContext:
     ctx.load_verify_locations(cadata=generate_ca_bundle())
     ctx.minimum_version = ssl.TLSVersion.TLSv1_2
     ctx.set_ciphers("DEFAULT")
+    ctx.verify_mode = ssl.CERT_REQUIRED
 
     try:
         ctx.hostname_checks_common_name = False
@@ -64,4 +107,7 @@ __all__ = (
     "root_pem_certificates",
     "generate_ca_bundle",
     "create_default_ssl_context",
+    "__version__",
+    "VERSION",
+    "RUSTLS_LOADED",
 )
