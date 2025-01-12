85062fc8faa61e73cdeb3e40ee99564731ee2261
Ahmed TAHRI <tahri.ahmed@proton.me>
2025-01-12 06:07:03 +0100

    :arrow_up: update rustls-native-certs from 0.7 to 0.8

--- src/lib.rs
+++ src/lib.rs
@@ -5,15 +5,14 @@ use pyo3::{prelude::*, types::PyBytes};
 #[pyfunction]
 fn root_der_certificates(py: Python) -> PyResult<Vec<Bound<'_, PyBytes>>> {
     let mut roots = Vec::new();
-    let certs = rustls_native_certs::load_native_certs();
 
-    if certs.is_err() {
-        return Err(PyRuntimeError::new_err(
-            "unable to extract root certificates",
-        ));
+    let container = rustls_native_certs::load_native_certs();
+
+    if container.certs.is_empty() {
+        return Err(PyRuntimeError::new_err("Failed to load native cert store"));
     }
 
-    for cert in certs.unwrap() {
+    for cert in container.certs {
         roots.push(PyBytes::new(py, cert.as_ref()));
     }
 
