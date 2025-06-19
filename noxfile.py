59a86c37ab87dd25c14a81e55f03c10a71ec6975
Ahmed TAHRI <tahri.ahmed@proton.me>
2025-06-19 07:41:40 +0200

    :heavy_check_mark: add noxfile

---
+++ noxfile.py
@@ -0,0 +1,41 @@
+from __future__ import annotations
+
+import os
+import nox
+
+
+@nox.session(python=["3.7", "3.8", "3.9", "3.10", "3.11", "3.12", "3.13", "pypy"])
+def test(session: nox.Session) -> None:
+    # Install deps and the package itself.
+    session.install("-r", "requirements-dev.txt")
+
+    # Show the pip version.
+    session.run("pip", "--version")
+    session.run("python", "--version")
+
+    session.run(
+        "python",
+        "-m",
+        "coverage",
+        "run",
+        "--parallel-mode",
+        "-m",
+        "pytest",
+        "-v",
+        "-ra",
+        f"--color={'yes' if 'GITHUB_ACTIONS' in os.environ else 'auto'}",
+        "--tb=native",
+        "--durations=10",
+        "--strict-config",
+        "--strict-markers",
+        *(session.posargs or ("tests/",)),
+        env={
+            "PYTHONWARNINGS": "always::DeprecationWarning",
+        },
+    )
+
+
+@nox.session
+def lint(session: nox.Session) -> None:
+    session.install("pre-commit")
+    session.run("pre-commit", "run", "--all-files")
